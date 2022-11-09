2d8368a21da1494b47bd4b5450dd33725cca4ada
brunopaiva1 <brunopaivamaster10@gmail.com>
2022-11-08 21:39:23 -0300

    formatacao2

---
+++ formatacao2.py
@@ -0,0 +1,16 @@
+#formatação de variáveis em python
+a = 200
+b = 300
+c = 600
+
+soma = a + b + c
+subtracao = a - b - c
+divisao = c / b
+multiplicacao = a * b * c
+resto = a % b & b % c
+
+print(f'Soma: {soma}')
+print(f'Subtração: {subtracao}')
+print(f'Divisão: {divisao}')
+print(f'Multiplicação: {multiplicacao}')
+print(f'Resto: {resto}')
